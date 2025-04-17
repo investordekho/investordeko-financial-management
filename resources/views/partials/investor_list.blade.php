a4596950dbb054afd345aab19803ae152620c76e
Pooja Dhage <investordekhopoojad@gmail.com>
2025-04-17 17:06:21 +0530

    all changes

--- resources/views/partials/investor_list.blade.php
+++ resources/views/partials/investor_list.blade.php
@@ -1,3 +1,7 @@
+@php 
+    $isSubscribed = isset($subscriber) && $subscriber?->is_subscribed;
+@endphp
+
 @if($investors->count())
     @php 
         $isSubscribed = $subscriber && $subscriber->is_subscribed;
