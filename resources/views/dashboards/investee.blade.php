08b7b3f6b4b0da5cce0917960c5cb83cf777d40a
Pooja Dhage <investordekhopoojad@gmail.com>
2025-04-24 13:20:27 +0530

    all changes

--- resources/views/dashboards/investee.blade.php
+++ resources/views/dashboards/investee.blade.php
@@ -273,9 +273,9 @@
 
                             <!-- Display investor results -->
                             <div id="investorList">
-                                <!-- @include('partials.investor_list', ['investors' => $investors]) -->
+                                @include('partials.investor_list', ['investors' => $investors]) 
                             
-                                <!-- // echo $investors; -->
+                                <!--  echo $investors; -->
                             
                                 <!-- <pre>{{ print_r($investors, true) }}</pre> -->
                             </div>
