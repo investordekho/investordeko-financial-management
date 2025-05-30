2bc09ee81c6ea6eb8b7387d28763d98685c43f8f
Pooja Dhage <investordekhopoojad@gmail.com>
2025-05-30 18:47:39 +0530

    all changes

--- storage/framework/views/a9fb9720f2b7ad7b115df0d429368fd8.php
+++ storage/framework/views/a9fb9720f2b7ad7b115df0d429368fd8.php
@@ -46,9 +46,9 @@
             <!-- Main Content -->
             <div class="col-lg-9">
                 <h1 class="mb-4">Investor Dekho Private Limited Terms and Conditions</h1>
-                <p>Investor Dekho Private Limited ("Investor Dekho," "us," "our," or "we") provides this website to you under the following terms and conditions (this "Agreement" or "Terms of Use"). Our Privacy Policy (as posted on our website), which explains how we collect and use information from visitors to our website ("Users"), is incorporated by reference in these Terms of Use. By using our website (the "Site") or submitting any information to us, you consent to and agree to be bound by these Terms of Use and our Privacy Policy. By using the Site in any manner, including but not limited to visiting or browsing the Site, you agree to be bound by this Agreement, the Privacy Policy, and all other operating rules, policies, and procedures that may be published by us from time to time on the Site, each of which is incorporated by reference and each of which may be updated by us from time to time without notice to you. Your use of the Site is governed by the version of this Agreement in effect on the date of use.</p
+                <p>Investor Dekho Private Limited ("Investor Dekho," "us," "our," or "we") provides this website to you under the following terms and conditions (this "Agreement" or "Terms of Use"). Our Privacy Policy (as posted on our website), which explains how we collect and use information from visitors to our website ("Users"), is incorporated by reference in these Terms of Use. By using our website (the "Site") or submitting any information to us, you consent to and agree to be bound by these Terms of Use and our Privacy Policy. By using the Site in any manner, including but not limited to visiting or browsing the Site, you agree to be bound by this Agreement, the Privacy Policy, and all other operating rules, policies, and procedures that may be published by us from time to time on the Site, each of which is incorporated by reference and each of which may be updated by us from time to time without notice to you. Your use of the Site is governed by the version of this Agreement in effect on the date of use.</p>
 
-<p>>Our website can be accessed from the domain name: www.investordekho.com</p>
+<p>Our website can be accessed at: www.investordekho.in</p>
 
                 <section id="account-registration">
                     <h2 class="fw-bold">Account Registration</h2>
@@ -57,35 +57,35 @@
 <p>You may access some areas of our site(s) without registering. However, to access certain features or post content on the Site, you must register to create an account ("Account"). You must complete the registration process by providing us with current, complete, and accurate information. If you choose to register, you will need to provide a username and password as well as other information that is part of our registration process. You will not share your password, let anyone else access your Account, or do anything else that might jeopardize the security of your Account. We have the right to cancel any account at any time if we believe you are not in compliance with any or all of the terms.</p>
                 </section>
 
-                <section id="social-media">
+                <!-- <section id="social-media">
                     <h2 class="fw-bold">Social Media</h2>
                     <p>You may register with Investor Dekho through your account with certain third-party social networking services, including Facebook, LinkedIn, and Twitter (collectively, "SNS"). When you register through your SNS account, you will be asked to log in to the Site using your SNS account credentials. By creating an Account via your account with an SNS, you allow Investor Dekho to access your SNS account information and agree to abide by the applicable terms and conditions of your SNS in your use of the Site via such SNS.</p>
-                </section>
+                </section> -->
 
                 <section id="user-content">
                     <h2 class="fw-bold">User Content</h2>
                     <p>The Site may allow Users to post or send information, text, data, photographs, audio, video, images, graphics, logos, trademarks, service marks, and other materials (collectively “User Content”) to or through the Site. User understands that all of the User Content and the consequences of such posting of User Content is User’s sole responsibility and is subject to the Terms of Use and the Investor Dekho Privacy Policy.</p>
 
-<p>By posting, uploading, or otherwise distributing the User Content on the Site, User hereby grants (and represents and warrants that User has all necessary rights to grant) to Investor Dekho a perpetual, sublicenseable, transferable, worldwide, non-exclusive, royalty-free, fully paid license to all User’s rights in the User Content (including moral rights) for all purposes, including but not limited to use, copy, reproduce, modify, adapt, publish, edit, translate, create derivative works from, transmit, distribute, display, perform, or derive revenue or other remuneration from such User Content and incorporate such User Content into other works in any form, media, or technology. By providing User Content to the Site, User hereby permits Investor Dekho to identify User as the provider of such User Content in any form, media, and technology.</p>
+                    <p>By posting, uploading, or otherwise distributing the User Content on the Site, User hereby grants (and represents and warrants that User has all necessary rights to grant) to Investor Dekho a perpetual, sublicenseable, transferable, worldwide, non-exclusive, royalty-free, fully paid license to all User’s rights in the User Content (including moral rights) for all purposes, including but not limited to use, copy, reproduce, modify, adapt, publish, edit, translate, create derivative works from, transmit, distribute, display, perform, or derive revenue or other remuneration from such User Content and incorporate such User Content into other works in any form, media, or technology. By providing User Content to the Site, User hereby permits Investor Dekho to identify User as the provider of such User Content in any form, media, and technology.</p>
 
-<p>User represents and warrants that the User Content does not infringe the copyright, trademark, publicity/privacy right, or other intellectual property or proprietary right of any third party and that User has paid and will pay in full any fees or other payments that may be related to the use of User’s User Content.</p>
+                    <p>User represents and warrants that the User Content does not infringe the copyright, trademark, publicity/privacy right, or other intellectual property or proprietary right of any third party and that User has paid and will pay in full any fees or other payments that may be related to the use of User’s User Content.</p>
 
-<p>User understands that by accessing the Site, User may be exposed to User Content that may be offensive or objectionable to User. Investor Dekho may, but is not obligated to, preview or review any User Content and, in its sole discretion, may block or remove from the Site, without notice, any User Content that violates the Terms of Use or is otherwise objectionable. However, failure to block or remove any User Content is not an endorsement, warranty, representation, or guarantee regarding such User Content.</p>
+                    <p>User understands that by accessing the Site, User may be exposed to User Content that may be offensive or objectionable to User. Investor Dekho may, but is not obligated to, preview or review any User Content and, in its sole discretion, may block or remove from the Site, without notice, any User Content that violates the Terms of Use or is otherwise objectionable. However, failure to block or remove any User Content is not an endorsement, warranty, representation, or guarantee regarding such User Content.</p>
 
-<p>User agrees that Investor Dekho has no liability or responsibility for the storage or deletion of any User Content that User submits or posts through the Site.</p>
+                    <p>User agrees that Investor Dekho has no liability or responsibility for the storage or deletion of any User Content that User submits or posts through the Site.</p>
                 </section>
 
                 <section id="subscription-plan">
                     <h2 class="fw-bold">Subscription Plan and Payment</h2>
-                    <p>There are several different types of subscriptions available to you. Depending on which of the below subscription services you select at the time of registering with Investor Dekho, the scope of services and payment terms will vary. All payments in favor of Investor Dekho are non-refundable, for any reason.</p>
+                    <p>There are several different types of subscriptions available to you. Depending on which of the subscription services you select at the time of registering with Investor Dekho, the scope of services and payment terms will vary. All payments in favor of Investor Dekho are non-refundable, for any reason.</p>
 
 <p>Access to all these services requires registration.</p>
 
 <p>While some services would be free for all our users, some services can be accessed only by way of subscription. Some offerings will be provided on an "as and when required basis" on the payment of requisite fees.</p>
 
-<p>Information about subscription plans and prices will be provided on the Site while requesting a subscription to a service. You agree to pay the price for the subscription or service as per the pricing schedule indicated on the Site. At the time of account creation or subscription, you need to provide accurate information regarding your account, including valid credit card or debit card or net banking credentials, and you must certify that you are over 18 years of age and legally able to enter into a valid and binding agreement with Investor Dekho.</p>
+<p>Information about subscription plans and prices will be provided on the Site while requesting a subscription to a service. You agree to pay the price for the subscription or service as per the pricing schedule indicated on the Site. At the time of account creation or subscription, you need to provide accurate information regarding your account and you must certify that you are over 18 years of age and legally able to enter into a valid and binding agreement with Investor Dekho.</p>
 
-<p>The term of your subscription shall be year-to-year or as specified in the plan. Your subscription will commence on the day you enter into this Agreement by entering valid credit card or other payment information on the Site. You may not cancel your subscription following receipt of payment by Investor Dekho (unless you are otherwise able to do so in accordance with your statutory rights).</p>
+<p>The term of your subscription shall be 90 days or as specified in the plan. Your subscription will commence on the day you enter into this Agreement by entering valid credit card or other payment information on the Site. You may not cancel your subscription following receipt of payment by Investor Dekho (unless you are otherwise able to do so in accordance with your statutory rights).</p>
 
 <p>You agree that Investor Dekho will not be liable for any loss caused by any unauthorized use of your credit card or any other method of payment by a third party in connection with the use of the Site. You waive your right to dispute any payment made into your account and you will bear all costs.</p>
 
@@ -151,11 +151,11 @@
                     <h2 class="fw-bold">Grievance Officer</h2>
                     <p><p>In accordance with the Information Technology Act 2000 and rules made thereunder, the name and contact details of the Grievance Officer are provided below:</p>
 
-<p><strong>Name:</strong> [Grievance Officer's Name]</p>
-<p><strong>Designation:</strong> Director</p>
-<p><strong>Phone:</strong> [Grievance Officer's Phone Number]</p>
-<p><strong>Email:</strong> <a href="mailto:[Grievance Officer's Email Address]">[Grievance Officer's Email Address]</a></p>
-<p><strong>Office Hours:</strong> [Office Hours]</p>
+<p><strong>Name:</strong> Pooja Bhagwan Dhage </p>
+<p><strong>Designation:</strong> Assistant Manager Software Developer</p>
+<p><strong>Phone:</strong> +022 31561436 </p>
+<p><strong>Mail:</strong> <a href="mailto:pooja.dhage@financialmindss.com">pooja.dhage@financialmindss.com"</a></p>
+<p><strong>Office Hours:</strong> 9 AM to 6 PM</p>
 </p>
                 </section>
 
@@ -183,7 +183,7 @@
 
                 <section id="jurisdiction">
                     <h2 class="fw-bold">Jurisdiction</h2>
-                    <p>The Terms of Use shall be governed by the laws of India, and the courts at [Your Location] shall have exclusive jurisdiction with respect to any dispute arising hereunder.</p>
+                    <p>The Terms of Use shall be governed by the laws of India, and the courts at Mumbai shall have exclusive jurisdiction with respect to any dispute arising hereunder.</p>
                 </section>
 
                 <section id="international-use">
